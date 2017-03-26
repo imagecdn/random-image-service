8afb7fed604308f0d25e4d071a4b36dce8bb8925
Alex Wilson <a@ax.gy>
2017-03-26 15:31:21 +0100

    A little more robust.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const {send, sendError} = require('micro')
+const express = require('express')
+const cors = require('cors')
 const fetch = require('isomorphic-fetch')
+const app = express()
 
-module.exports = (req, res) => {
+app.use(cors())
+
+app.get('/v1/image', (req, res, next) => {
   fetch('https://source.unsplash.com/category/buildings/1920x1200')
-    .then(imgRes => {
-      res.setHeader('Location', imgRes.url)      
-      send(res, 302, imgRes.url)
-    })
-    .catch(err => sendError(req, res, err))
+    .then(imgRes => _redirect(res, imgRes.url))
+    .catch(err => next(err))
+})
+
+app.get('/__health', (req, res) => {
+  res.json({status:'OK'})
+})
+
+app.get('/*', (req, res) => {
+  _redirect(res, 'https://responsiveimages.io/')
+})
+
+const _redirect = (res, url) => {
+  res.setHeader('Location', url)
+  res.status(302).send(url)
 }
+
+app.listen(process.env.PORT || 3000)
