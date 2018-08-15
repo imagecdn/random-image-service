256c23289d2e93a7394900de7f4943bec3a9f1e4
Alex Wilson <a@ax.gy>
2018-08-15 19:00:13 +0200

    feat(providers): WIP refactoring work in splitting providers.

---
+++ src/provider/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    s3: require('./s3'),
+    unsplash: require('./unsplash')
+}
---
+++ src/provider/unsplash.js
@@ -0,0 +1,22 @@
+const BaseProvider = require('./base-provider')
+const fetch = require('isomorphic-fetch')
+
+class Unsplash extends BaseProvider
+{
+    constructor() {
+        super()
+        this._defaultResponseBody.provider = 'unsplash'
+        this._defaultResponseBody.license = 'CC0'
+        this._defaultResponseBody.terms = 'https://unsplash.com/terms'
+    }
+
+    randomImage(query) {
+        return fetch(`https://source.unsplash.com/category/${query.category}/${query.size.width}x${query.size.height}`)
+        .then(res => this._normalizeResponse({
+            url: res.url,
+            size: query.size
+        }))
+    }
+}
+
+module.exports = Unsplash
