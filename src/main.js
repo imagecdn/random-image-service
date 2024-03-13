7d674384b2806461bc0231730bebc8e207e6b8f3
Alex Wilson <alex@alexwilson.tech>
2024-03-13 16:49:59 +0000

    feat(deps): Major upgrade to Node 20

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const expressHandlebars = require('express-handlebars')
+const { engine: expressHandlebars } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
