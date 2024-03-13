8dbe550206c23523c6ed1c08f0e2b80495f8b02f
Alex Wilson <alex@alexwilson.tech>
2024-03-13 16:49:59 +0000

    feat(deps): Major upgrade to Node 20

--- src/provider/unsplash.js
+++ src/provider/unsplash.js
@@ -1,28 +1,29 @@
-const fetch = require('isomorphic-fetch')
-const BaseProvider = require('./base-provider')
+const BaseProvider = require("./base-provider");
 
-class Unsplash extends BaseProvider
-{
-    constructor(provider) {
-        super(provider)
-        this._defaultResponseBody.provider = 'unsplash'
-        this._defaultResponseBody.license = 'CC0'
-        this._defaultResponseBody.terms = 'https://unsplash.com/terms'
-    }
+class Unsplash extends BaseProvider {
+  constructor(provider) {
+    super(provider);
+    this._defaultResponseBody.provider = "unsplash";
+    this._defaultResponseBody.license = "CC0";
+    this._defaultResponseBody.terms = "https://unsplash.com/terms";
+  }
 
-    static get defaultable() {
-        return true
-    }
+  static get defaultable() {
+    return true;
+  }
 
-    randomImage(query) {
-        const height = query.size.height || 1080
-        const width = query.size.width || 1920
-        return fetch(`https://source.unsplash.com/category/${query.category}/${width}x${height}`)
-        .then(res => this._normalizeResponse({
-            url: res.url,
-            size: {height, width}
-        }))
-    }
+  randomImage(query) {
+    const height = query.size.height || 1080;
+    const width = query.size.width || 1920;
+    return fetch(
+      `https://source.unsplash.com/category/${query.category}/${width}x${height}`,
+    ).then((res) =>
+      this._normalizeResponse({
+        url: res.url,
+        size: { height, width },
+      }),
+    );
+  }
 }
 
-module.exports = Unsplash
+module.exports = Unsplash;
